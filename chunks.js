e1186ca6ce5f7d752d813aca9cd17f28aa8384b5
Adriano Regis <adbrum@outlook.com>
2019-01-23 19:36:02 +0000

    Add shard index

--- chunks.js
+++ chunks.js
@@ -2,20 +2,29 @@ use cinema
 
 db.ratings.createIndex( {  movieId : "hashed" }, {unique: false} )
 sh.shardCollection( "cinema.ratings", { movieId : "hashed" } )
-sh.moveChunk("cinema.ratings", { movieId : "hashed" }, "shard01")
+
+db.links.createIndex( {  movieId : "hashed" }, {unique: false} )
+sh.shardCollection( "cinema.links", { movieId : "hashed" } )
 
 db.movies.createIndex( {  movieId : "hashed" }, {unique: false} )
 sh.shardCollection( "cinema.movies", { movieId : "hashed" } )
-sh.moveChunk("cinema.movies", { movieId : "hashed" }, "shard02")
 
 db.tags.createIndex( {  movieId : "hashed" }, {unique: false} )
 sh.shardCollection( "cinema.tags", { movieId : "hashed" } )
-sh.moveChunk("cinema.tags", { movieId : "hashed" }, "shard02")
 
 db.genome_scores.createIndex( {  movieId : "hashed" }, {unique: false} )
 sh.shardCollection( "cinema.genome_scores", { movieId : "hashed" } )
-sh.moveChunk("cinema.genome_scores", { movieId : "hashed" }, "shard03")
 
 db.genome_tags.createIndex( {  tagId  : "hashed" }, {unique: false} )
 sh.shardCollection( "cinema.genome_tags", { tagId  : "hashed" } )
+
+
+sh.moveChunk("cinema.links", { tagId  : "hashed" }, "shard01")
+sh.moveChunk("cinema.ratings", { movieId : "hashed" }, "shard01")
+sh.moveChunk("cinema.movies", { movieId : "hashed" }, "shard02")
+sh.moveChunk("cinema.tags", { movieId : "hashed" }, "shard02")
+sh.moveChunk("cinema.genome_scores", { movieId : "hashed" }, "shard03")
 sh.moveChunk("cinema.genome_tags", { tagId  : "hashed" }, "shard03")
+
+
+//db.printShardingStatus(true)
